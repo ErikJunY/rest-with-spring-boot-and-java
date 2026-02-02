c52e33f643ec6adb2848a38192258dda3ea2f751
Erik Jun <erikjunyamazaki@gmail.com>
2026-02-02 08:01:55 -0300

    Creating the Person, PersonController, and PersonServices classes. Dependency injection with @Service and @Autowired.

--- rest-with-spring-boot-and-java/src/main/java/io/github/erikjuny/model/Greeting.java
+++
@@ -1,3 +0,0 @@
-package io.github.erikjuny.model;
-
-public record Greeting(long id, String content) {}
---
+++ rest-with-spring-boot-and-java/src/main/java/io/github/erikjuny/services/PersonServices.java
@@ -0,0 +1,28 @@
+package io.github.erikjuny.services;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Service;
+
+import io.github.erikjuny.model.Person;
+
+@Service
+public class PersonServices {
+	
+	private final AtomicLong counter = new AtomicLong();
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+	public Person findById(String id) {
+		logger.info("Finding one Person!");
+		
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Erik");	
+		person.setLastName("Jun");
+		person.setAddress("Barueri - SP");
+		person.setGender("Male");
+		return person;
+		
+	}
+}
