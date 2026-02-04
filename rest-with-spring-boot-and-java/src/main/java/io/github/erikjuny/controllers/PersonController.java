641101d6baf056525061e2a26de2872fbac859e2
Erik Jun <erikjunyamazaki@gmail.com>
2026-02-04 13:59:56 -0300

    Feat: Criado classes PersonService e novos Endpoints

--- rest-with-spring-boot-and-java/src/main/java/io/github/erikjuny/controllers/PersonController.java
+++ rest-with-spring-boot-and-java/src/main/java/io/github/erikjuny/controllers/PersonController.java
@@ -1,5 +1,7 @@
 package io.github.erikjuny.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,5 +25,18 @@ public class PersonController {
 	public Person findById(@PathVariable("id") String id) {
 		return service.findById(id);
 	}
+	
+	@RequestMapping(method = RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll() {
+		return service.findAll();
+	}
+	
+	@RequestMapping(method = RequestMethod.POST,
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+		public Person create(Person person) {
+			return service.create(person);
+		}
 
 }
--- rest-with-spring-boot-and-java/src/main/java/io/github/erikjuny/services/PersonServices.java
+++ rest-with-spring-boot-and-java/src/main/java/io/github/erikjuny/services/PersonServices.java
@@ -1,5 +1,7 @@
 package io.github.erikjuny.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -12,7 +14,18 @@ public class PersonServices {
 	
 	private final AtomicLong counter = new AtomicLong();
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
-
+	
+	public List<Person> findAll() {
+		logger.info("Finding all People!!");
+		List<Person> persons = new ArrayList<Person>();
+		for(int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;
+		
+	}
+	
 	public Person findById(String id) {
 		logger.info("Finding one Person!");
 		
@@ -24,5 +37,20 @@ public class PersonServices {
 		person.setGender("Male");
 		return person;
 		
+	}
+	public Person create(Person person) {
+		logger.info("Creating one Person!");
+		return person;
+		
+	}
+	
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("FirstName " + i);	
+		person.setLastName("LastName " + i);
+		person.setAddress("Some Address in Brasil");
+		person.setGender("Male");
+		return person;
 	}
 }
